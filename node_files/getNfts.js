45df0c390ee498ca0fae59bb67b5053a48a9dec8
Kevin Liu <mangoquarterzip@Kevins-MacBook-Air.local>
2023-08-12 03:16:52 +0100

    added working node getnft covalent script

---
+++ node_files/getNfts.js
@@ -0,0 +1,28 @@
+
+/// this is working!
+const apiUrl = 'https://api.covalenthq.com/v1/eth-goerli/address/0x1E117008E1a544Bbe12A2d178169136703430190/balances_nft/?with-uncached=true';
+const headers = new Headers();
+headers.append('Authorization', 'Bearer cqt_rQPkghTgFxgYW478DmRvDGRft49t');
+
+// Make the API request
+fetch(apiUrl, { headers })
+  .then(response => response.json())
+  .then(data => {
+    if (data.data && data.data.items) {
+      const nftItems = data.data.items;
+
+      // Loop through nft_data and extract token_id and token_url
+      for (const nftItem of nftItems) {
+        for (const nftData of nftItem.nft_data) {
+          const tokenId = nftData.token_id;
+          const tokenUrl = nftData.token_url;
+          console.log(`Token ID: ${tokenId}, Token URL: ${tokenUrl}`);
+        }
+      }
+    } else {
+      console.log('No NFT data found.');
+    }
+  })
+  .catch(error => {
+    console.error('Error fetching NFT data:', error);
+  });
--- notes.md
+++ notes.md
@@ -1,1 +1,11 @@
 With covalent, should be made clearer in the docs that you have to include Bearer in the API key when making a request
+
+swagger doesnt work
+
+TO do list:
+
+Create express js server, make rq to covalent for the address of window.ethereum, then display that as eligible NFTs to be used, also do for tokens, filter out scam tokens
+
+create deploy page/open a hedge page
+
+i need to get data[0]items[0]]nft_data[i]token_id[i]
